cf71692a4edb3a0ed37cc49749a6496ca63c2e27
unknown <angel.maciej@wp.pl>
2023-05-15 13:53:13 +0200

    fix

---
+++ script.js
@@ -0,0 +1,7 @@
+let page = document.querySelector(".page");
+
+document.querySelector(".box1").addEventListener("click", _=>{
+    page.style.width = "70vw";
+    page.style.height = "70vh";
+    page.style.opacity = 1;
+})
