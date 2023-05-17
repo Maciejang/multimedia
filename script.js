538269d542c1196070947370ec4e4a7486f2cfa7
unknown <angel.maciej@wp.pl>
2023-05-17 10:49:29 +0200

    close_button

--- script.js
+++ script.js
@@ -4,4 +4,16 @@ document.querySelector(".box1").addEventListener("click", _=>{
     page.style.width = "70vw";
     page.style.height = "70vh";
     page.style.opacity = 1;
+    document.querySelector(".close").style.width = "2vw";
+    document.querySelector(".close").style.height = "2vw";
+
+})
+
+document.querySelector(".close").addEventListener("click", _=>{
+    page.style.width = "0vw";
+    page.style.height = "0vh";
+    page.style.opacity = 0;
+    document.querySelector(".close").style.width = "0vw";
+    document.querySelector(".close").style.height = "0vh";
+
 })
