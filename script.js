93fc5802ae6cd5190cc4fcd19765631acf6205e6
unknown <angel.maciej@wp.pl>
2023-05-17 11:56:47 +0200

    animations

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 let page = document.querySelector(".page");
+let opened = false
 
-document.querySelector(".box1").addEventListener("click", _=>{
+function openPage(animNum){
+    if(opened == true){
+        return;
+    }
+    opened = true;
     page.style.width = "70vw";
     page.style.height = "70vh";
     page.style.opacity = 1;
-    document.querySelector(".close").style.width = "2vw";
-    document.querySelector(".close").style.height = "2vw";
+    if(screen.width >= 1000){
+        document.querySelector(".close").style.width = "2vw";
+        document.querySelector(".close").style.height = "2vw";
+    }
+    else if(screen.width < 1000 && screen.width > 600){
+        document.querySelector(".close").style.width = "4vw";
+        document.querySelector(".close").style.height = "4vw";
+    }
+    else if(screen.width <= 600){
+        document.querySelector(".close").style.width = "7vw";
+        document.querySelector(".close").style.height = "7vw";
+    }
+    page.classList.add("showOpenAnim" + animNum);
+    setTimeout(_=>{
+        page.classList.remove("showOpenAnim" + animNum);
+    }, 800)
+}
 
+document.querySelector(".box1").addEventListener("click", _=>{
+    let rand = Math.floor(1+Math.random()*2);
+    openPage(rand);
 })
 
 document.querySelector(".close").addEventListener("click", _=>{
@@ -15,5 +38,5 @@ document.querySelector(".close").addEventListener("click", _=>{
     page.style.opacity = 0;
     document.querySelector(".close").style.width = "0vw";
     document.querySelector(".close").style.height = "0vh";
-
+    opened = false;
 })
