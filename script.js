1c3d62909ff98dd9846999c76d7885c9e0bae64a
Maciejang <97472943+Maciejang@users.noreply.github.com>
2023-06-08 11:56:49 +0200

    commit

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ let box = document.querySelectorAll(".box");
 let backgroundAudio = document.querySelector(".bgMusic");
 let volumeSlider = document.querySelector(".volumeBar");
 let opened = false
-let pageContentArray = [`lorem ipsum1`, `lorem ipsum2`, `lorem ipsum3`, `lorem ipsum4`, `lorem ipsum5`, `lorem ipsum6`];
+let pageContentArray = [
+`Cafe frappe, czyli kawa mrożona (zwana również kawą grecką). W przeciwieństwie do  poprzednich przepisów na kawy na bazie espresso, tutaj wystarczy nam kawa rozpuszczalna, chociaż lepszego kopa da normalna. Kawa bardzo popularna w letnie upały, często podawana z lodami, śmietaną, bądź po prostu z dolanym zimnym mlekiem i kostkami lodu.`, 
+`Caffe Latte Wyjątkowo mleczna i łagodna kawa podawana w wysokich szklankach podbiła nie tylko włoskie serca, ale zyskała popularność wśród ludzi na całym świecie. Jest to podstawa i zazwyczaj pija się ją nie by się pobudzić, ale aby wypić do deseru itp. Bardzo dobrze komponuje się z wszelkimi syropami itp.`, 
+`Espresso Czyli idealny sposób na szybkie pobudzenie! Espresso to napar, który przygotowywany jest z drobno zmielonych ziaren kawy w specjalnym ekspresie. Nazwa najpewniej wywodzi się od włoskiego przymiotnika „espressivo”, co znaczy „wyrazisty”. I to właśnie doskonale określa charakter tej kawy – wyrazisty smak, zapach i barwa! Typowo włoskie espresso parzy się najczęściej z mieszanki różnych gatunków kaw. Na powierzchni doskonale zaparzonej kawy unosi się gęsta orzechowo-brązowa pianka – „crema”.`, 
+`Flat white to kawa z dodatkiem mleka, ale jej przygotowanie odbiega od tego, co przychodzi nam do głowy. Flat White przyrządza się nieco inaczej niż zwyczajne wlanie mleka. Podwójne espresso zalewa się gorącym, tylko lekko spienionym mlekiem (najlepiej pełnotłustym!). W przeciwieństwie do Caffe Latte tutaj pianki jest mniej przez co jest „flat”, czyli z angielskiego płaska.`, 
+` Mocha Kawa stworzona wręcz dla miłośników połączenia kawy i czekolady. Jest to połączenie czarnej mocnej kawy i czekolady, nie ma w regule jaką czekoladę podawać. Można mleczną, białą, albo jak oryginalnie gorzką. Najlepszy efekt jest z czekoladą rozpuszczoną wcześniej w kąpieli wodnej.`, 
+`Cappuccino to dobrze znany wszystkim włoski napój kawowy, którego głównymi składnikami są espresso oraz spienione mleko. Ta pyszna pianka oprószona jest często w kawiarniach itp. kakao lub sypką czekoladą. Sekretem idealnego cappuccino jest odpowiednio spienione mleko, które sprawia, że pianka jest kremowa i pozbawiona widocznych pęcherzyków powietrza. Ciekawostka! Włoskie standardy jasno określają grubość pianki – powinna wynosić od 1 do 2 centymetrów. `];
 
 //page functionality
 function openPage(animNum, content){
@@ -31,6 +37,7 @@ function changeBGvolume(volumeValue){
 changeBGvolume(volumeSlider.value);
 volumeSlider.addEventListener("change", _=>{
    changeBGvolume(volumeSlider.value)
+   backgroundAudio.play();
 })
 //page close button
 document.querySelector(".close").addEventListener("click", _=>{
